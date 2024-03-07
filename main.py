5495e87773902a4735903b45ecd4a016769bfd79
ahmadGalangLawendatu <132432181+ahmadGalangLawendatu@users.noreply.github.com>
2024-03-07 18:15:04 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from modules.dvd import Dvd
+
+dvd_object = Dvd(title='ngetes',
+                 upc='',
+                 subject='',
+                 actor='',
+                 director='',
+                 genre='horor')
+print(dvd_object.genre)
