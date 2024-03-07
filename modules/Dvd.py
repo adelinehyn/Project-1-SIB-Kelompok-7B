15bf49cec0c7073c039819abff90049a29fe7700
ahmadGalangLawendatu <132432181+ahmadGalangLawendatu@users.noreply.github.com>
2024-03-08 00:07:02 +0800

    Update Dvd.py

--- modules/Dvd.py
+++ modules/Dvd.py
@@ -2,8 +2,8 @@ from modules.library_item import LibraryItem
 
 
 class Dvd(LibraryItem):
-    def __init__(self, title, upc, subject, actor, director, genre):
+    def __init__(self, title, upc, subject, actors, directors, genre):
         super().__init__(title, upc, subject)
-        self.actor = actor
-        self.director = director
+        self.actors = actors
+        self.directors = directors
         self.genre = genre
