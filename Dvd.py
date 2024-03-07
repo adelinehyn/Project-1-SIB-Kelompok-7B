8be714d88fb2b6c920f28d73b0caa00ab83648b6
ahmadGalangLawendatu <132432181+ahmadGalangLawendatu@users.noreply.github.com>
2024-03-07 18:23:14 +0800

    Delete Dvd.py

--- Dvd.py
+++
@@ -1,9 +0,0 @@
-from modules.library_item import LibraryItem
-
-
-class Dvd(LibraryItem):
-    def __init__(self, title, upc, subject, actor, director, genre):
-        super().__init__(title, upc, subject)
-        self.actor = actor
-        self.director = director
-        self.genre = genre
