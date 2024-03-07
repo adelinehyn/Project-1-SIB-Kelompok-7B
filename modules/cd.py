eff749b58ec0e5c553453dc3b9cfce5ce995de20
ahmadGalangLawendatu <132432181+ahmadGalangLawendatu@users.noreply.github.com>
2024-03-08 00:09:59 +0800

    Update and rename Cd.py to cd.py

--- modules/cd.py
+++ modules/cd.py
@@ -1,7 +1,7 @@
 from modules.library_item import LibraryItem
 
 
-class CD(LibraryItem):
+class Cd(LibraryItem):
     def __init__(self, title, upc, subject, artist):
         super().__init__(title, upc, subject)
         self.artist = artist
