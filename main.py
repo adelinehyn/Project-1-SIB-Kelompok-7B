01f47c17a023f15279d8c08f59ea7451d2a09116
Anita Primawati <142095066+AnitaPrimawati@users.noreply.github.com>
2024-03-07 17:28:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ dvd_object = Dvd(title='ngetes',
                  director='',
                  genre='horor')
 print(dvd_object.genre)
+
+from modules.library_item import LibraryItem
+
+class Book(LibraryItem):
+    def __init__(self, title, upc, subject, issbn, authors, dds_number):
+        super().__init__(title, upc, subject)
+        self.issbn = issbn
+        self.authors = authors
+        self.dds_number = dds_number
